f09c092d0f6098a7440145a01df6e76aab346071
Josip Marković <josip.markovic@factory.hr>
2020-05-28 18:51:17 +0200

    first commit

---
+++ NotesApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NotesApp
+//
+//  Created by Josip Marković on 28/05/2020.
+//  Copyright © 2020 Josip Marković. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
